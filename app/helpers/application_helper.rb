321c469f89ac22c978b04b92ba0b977c8b469c0d
Yukihiro Yoshida <greeeeeeen11@gmail.com>
2018-12-09 22:58:39 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Review App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
